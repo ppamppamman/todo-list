6e86a60da6926f194af6883307bd21a5e34c51f6
cchoongh <cndgh49@naver.com>
2021-04-11 22:14:08 +0900

    Add global.js

--- src/global.js
+++ src/global.js
@@ -3,6 +3,16 @@ let USER = 'Anonymous';
 const Global = {
   setUser: (user) => { USER = user },
   getUser: () => USER,
+  getInitialTodoData: () => { // TODO: right located?
+    return {
+      id: null,
+      author: '',
+      title: '',
+      content: '',
+      createDate: null,
+      updateDate: null,
+    }
+  },
 }
 
 export default Global;
