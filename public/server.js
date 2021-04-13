cc9d47266ea10f8c77ea6a87648f6324741a2297
cchoongh <cndgh49@naver.com>
2021-04-13 16:30:42 +0900

    Modify server side codes

--- public/server.js
+++ public/server.js
@@ -3,20 +3,25 @@
 // const router = jsonServer.router('db.json');
 // const middlewares = jsonServer.defaults();
 
+const path = require('path');
 const express = require('express');
 const app = express();
 
 // DB
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db/db.json');
+const adapter = new FileSync(path.join(__dirname, 'db/db.json'));
 const db = low(adapter);
 
 // server.use(middlewares);
 // server.use(router);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => { console.log('json-server is running.'); });
+app.get('/static/*', (req, res) => {
+  res.sendFile(path.join(__dirname, req.url));
+});
+
+app.listen(3000, () => { console.log('express server is running.'); });
