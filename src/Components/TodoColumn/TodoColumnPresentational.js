c1b2c961df8c8eb749fb7398545ef265dfef85ab
cchoongh <cndgh49@naver.com>
2021-04-13 16:52:13 +0900

    Fix the bug for not deleted new TodoCard

--- src/Components/TodoColumn/TodoColumnPresentational.js
+++ src/Components/TodoColumn/TodoColumnPresentational.js
@@ -11,7 +11,7 @@ function TodoColumnPresentational(props) {
         dispatch={props.dispatch}
         addTodo={props.addTodo}
         deleteTodo={props.deleteTodo}
-        viewState={data.createDate ? TodoCardViewState.NORMAL : TodoCardViewState.EDIT}
+        viewState={data.createTime ? TodoCardViewState.NORMAL : TodoCardViewState.EDIT}
         state={data}
       />);
   }
