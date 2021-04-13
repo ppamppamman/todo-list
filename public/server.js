6a722f8260b05ea4a1129b0068bf0a7ac1f4ff54
cchoongh <cndgh49@naver.com>
2021-04-13 22:54:49 +0900

    Implement compact server
    * not tested yet

--- public/server.js
+++ public/server.js
@@ -13,8 +13,8 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync(path.join(__dirname, 'db/db.json'));
 const db = low(adapter);
 
-// server.use(middlewares);
-// server.use(router);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -22,6 +22,36 @@ app.get('/', (req, res) => {
 
 app.get('/static/*', (req, res) => {
   res.sendFile(path.join(__dirname, req.url));
+});
+
+app.get('/column', (req, res) => {
+  res.json(db.get('columns').value());
+});
+
+app.post('/column', (req, res) => {
+  //TODO
+});
+
+app.get('/column/:id', (req, res) => {
+  //TODO
+});
+
+app.get('/column/:columnId/todo', (req, res) => {
+  res.json(db.get('todos').filter({ columnId: req.params.columnId }).value());
+});
+
+app.get('/column/:columnId/todo/:todoId', (req, res) => {
+  //TODO
+});
+
+app.post('/column/:columnId/todo', (req, res) => {
+  db.get('todos').push(req.body).write();
+  res.json({ success: true });
+});
+
+app.put('/column/:columnId/todo/:todoId', (req, res) => {
+  db.get('todos').find({ id: req.params.id }).assign(req.body).write();
+  res.json({ success: true });
 });
 
 app.listen(3000, () => { console.log('express server is running.'); });
