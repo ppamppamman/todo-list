70f9cafbcc7212f7edb1826baed55cbdcacadf98
cchoongh <cndgh49@naver.com>
2021-04-15 10:55:49 +0900

    Modify API

--- src/util/API.js
+++ src/util/API.js
@@ -1,56 +1,64 @@
 import Global from '../global.js';
 
 const API = {
-  getLogin: async ({ userId }) => {
-    // TODO: return nickname
+  get: {
+    userName: async ({ userId }) => {
+      // TODO: return nickname
+    },
+    column: async ({ columnId }) => {
+      const resourcePath = `${Global.getServerUrl()}/columns/${columnId}`;
+      const res = await fetch(resourcePath);
+      return res.json();
+    },
+    columns: async () => {
+      const resourcePath = `${Global.getServerUrl()}/${Global.getUser().id}/columns`;
+      const res = await fetch(resourcePath);
+      return res.json();
+    },
+    todo: async ({ todoId }) => { /* TODO */ },
+    todos: async ({ columnId }) => {
+      const resourcePath = `${Global.getServerUrl()}/columns/${columnId}/todos`;
+      const res = await fetch(resourcePath);
+      return res.json();
+    },
   },
-  getColumn: async ({ columnId }) => {
-    const resourcePath = `${Global.getServerUrl()}/columns/${columnId}`;
-    const res = await fetch(resourcePath);
-    return res.json();
+  post: {
+    column: async ({ columnData }) => {
+      const resourcePath = `${Global.getServerUrl()}/${columnData.userId}/column`
+      const res = await fetch(resourcePath, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(columnData)
+      });
+      return res.json();
+    },
+    todo: async ({ todoData }) => {
+      const resourcePath = `${Global.getServerUrl()}/columns/${todoDate.columnId}/todo`;
+      const res = await fetch(resourcePath, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todoData)
+      });
+      return res.json();
+    },
   },
-  getColumns: async () => {
-    const resourcePath = `${Global.getServerUrl()}/${Global.getUser().id}/columns`;
-    const res = await fetch(resourcePath);
-    return res.json();
+  patch: {
+    column: async ({ columnId }) => { /* TODO */ },
+    todo: async ({ columnId }) => {
+  
+    },
   },
-  getTodo: async ({ todoId }) => { /* TODO */ },
-  getTodos: async ({ columnId }) => {
-    const resourcePath = `${Global.getServerUrl()}/columns/${columnId}/todos`;
-    const res = await fetch(resourcePath);
-    return res.json();
-  },
-  postColumn: async ({ columnData }) => {
-    const resourcePath = `${Global.getServerUrl()}/${columnData.userId}/column`
-    const res = await fetch(resourcePath, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(columnData)
-    });
-    return res.json();
-  },
-  postTodo: async ({ todoData }) => {
-    const resourcePath = `${Global.getServerUrl()}/columns/${todoDate.columnId}/todo`;
-    const res = await fetch(resourcePath, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(todoData)
-    });
-    return res.json();
-  },
-  patchColumn: async ({ columnId }) => { /* TODO */ },
-  patchTodo: async ({ columnId }) => {
+  delete: {
+    deleteColumn: {
 
-  },
-  deleteColumn: {
-
-  },
-  deleteTodo: {
-
+    },
+    deleteTodo: {
+  
+    }
   }
 }
 
